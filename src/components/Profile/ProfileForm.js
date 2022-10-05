af5eaffcaaeb4776e2d2e6bc1ff189a27c8e3caf
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-05 10:56:39 +0300

    Add https request change password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,36 @@
 import classes from './ProfileForm.module.css';
+import {useRef, useContext } from "react";
+import authContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+    const newPasswordInputRef = useRef();
+    const authCtx = useContext(authContext);
+
+    const submitHandler = event => {
+            event.preventDefault();
+
+            const enteredNewPassword = newPasswordInputRef.current.value;
+
+           fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB5yrXG53j9G-ec262g-AkLzwBuaGuIPKk', {
+               method: 'POST',
+               body: JSON.stringify({
+                   idToken: authCtx.token,
+                   password: enteredNewPassword,
+                   returnSecureToken: false
+               }),
+               headers: {
+                   'Content-Type': 'application/json'
+               }
+           }).then(res => {
+
+           })
+    };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
